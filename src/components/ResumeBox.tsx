0798e383574952ca7702cd38570c2b21deabeefa
EllissaP <ellissa.peterson@gmail.com>
2025-01-01 19:55:53 -0600

    add github links

--- src/components/ResumeBox.tsx
+++ src/components/ResumeBox.tsx
@@ -1,13 +1,15 @@
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
 import documentPdf from '../../public/EllissaPetersonResume.pdf';
 
 function ResumeBox() {
   return (
     <>
       <div className="resume-box">
-        <div className="resume-box-text">
-          <Button href={documentPdf}>Checkout My Resume (PDF)</Button>
-        </div>
+      <ButtonGroup vertical className="resume-box-text">
+        <Button href={documentPdf} className="resume-box-list-item">My Resume (PDF)</Button>
+        <Button href="https://github.com/EllissaP" className="resume-box-list-item">GitHub (personal)</Button>
+        <Button href="https://github.com/EllissaPeterson" className="resume-box-list-item">GitHub (college)</Button>
+      </ButtonGroup>
       </div>
     </>
   );
